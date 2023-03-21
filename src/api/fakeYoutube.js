7ae16ebccf8b7567016368e6b417759e8ce27f61
김용민 <dydals3440@gmail.com>
2023-03-21 22:10:40 +0900

    Feat: Youtube API(postman, real youtube api)

--- src/api/fakeYoutube.js
+++ src/api/fakeYoutube.js
@@ -2,22 +2,20 @@ import axios from "axios";
 
 export default class FakeYoutube {
   // 생성할 떄 아무것도 전달해주지 않아도 됨.
-  constructor() {}
+  //   constructor() {}
   async search(keyword) {
     // # private은 클래스 내부적으로 호출이 가능하나 외부적으로는 호출 x
     return keyword ? this.#searchByKeyword(keyword) : this.#mostPopular();
   }
 
-  async #searchByKeyword(keyword) {
+  //   이미 지정된 검색 결과의 json을 사용할것이기에 (keyword) 가 필요 없기에 ()로 놨둔다.
+  async #searchByKeyword() {
     return axios
       .get(`/videos/search.json`)
-      .then((res) =>
-        res.data.items.then((items) =>
-          items.map((item) => ({ ...items, id: item.id.videoId }))
-        )
-      );
+      .then((res) => res.data.items)
+      .then((items) => items.map((item) => ({ ...item, id: item.id.videoId })));
   }
-  async #mostPopular(keyword) {
+  async #mostPopular() {
     return axios.get(`/videos/popular.json`);
   }
 }
--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -3,7 +3,7 @@ import { useQuery } from "react-query";
 // useParams를 이용해 검색 키워드를 표기
 import { useParams } from "react-router-dom";
 import FakeYoutube from "../api/fakeYoutube";
-import { search } from "../api/youtube";
+import Youtube from "../api/youtube";
 import VideoCard from "../components/VideoCard";
 
 export default function Videos() {
@@ -12,10 +12,14 @@ export default function Videos() {
     isLoading,
     error,
     data: videos,
-  } = useQuery(["videos", keyword], () => {
-    const youtube = new FakeYoutube();
-    youtube.search(keyword);
-  });
+  } = useQuery(
+    // 전체적인 videos라는 key안에, keyword별로 캐시가 되도록 만들어줌, 두번쨰 인자 어떻게 데이터 통신할 껀지 정의할 수 있는 콜백함수
+    ["videos", keyword],
+    () => {
+      const youtube = new Youtube();
+      youtube.search(keyword);
+    }
+  );
   return (
     <>
       <div>Videos {keyword ? `🔍${keyword}` : "🔥HotTrend Video"}</div>
